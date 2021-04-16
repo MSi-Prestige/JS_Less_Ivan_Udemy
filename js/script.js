3401989087fced9f430dab3482d8883fd2fe7d95
Ilja_JS <iljakristafenko@gmail.com>
2021-04-16 11:55:05 +0300

    First JS Lesson Ivan

---
+++ js/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// let number = 4.5;
+
+// const obj = {
+//     firstName: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+
+// console.log(obj.firstName);
+
+// let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(arr[3], arr[4]);
+
+// alert("hello Ilja");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("You have 18?", "");
+// console.log(typeof answer);
+
+// const answers = [];
+// answers[0] = prompt("Your name?", "");
+// answers[1] = prompt("Your second name?", "");
+// answers[2] = prompt("Your age?", "");
+
+// document.write(answers);
+// console.log(typeof (answers));
+
+// const user = prompt("That is your name?");
+
+// alert(`Hello, ${user}! Have are you?`);
+
+//!----------------Operators------------------------------
+
+console.log("arr" + " - object");
+console.log(4 + "5");
+//45
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+//11
+console.log(13 % 5);
+//3
+
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
+//true
+
+
+
+
+
