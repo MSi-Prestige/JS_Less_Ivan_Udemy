4b734b1846a8502a1bb6fa9fb3254221ee86f0d7
Ilja_JS <iljakristafenko@gmail.com>
2021-04-16 15:44:16 +0300

    DBMovie

--- js/scriptWork.js
+++ js/scriptWork.js
@@ -1,3 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("Один из последних фильмов ?", ""),
+    b = +prompt("На сколько вы его оцениваете ?", ""),
+    c = prompt("Один из последних фильмов ?", ""),
+    d = +prompt("На сколько вы его оцениваете ?", "");
+
+personalMovieDB.movies[a] = b; //! Запись в обьект - ключ (a) + значение (b).... а = ответ на вопрос ! а не номер.
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
