9ea2a4c54c0b63625cbd06d8ac7c3137302d1f04
Ilja_JS <iljakristafenko@gmail.com>
2021-04-17 20:15:15 +0300

    String NUmber

--- js/script.js
+++ js/script.js
@@ -57,57 +57,140 @@
 //true
 
 //! IF SWITCH
-const num = 50;
+// const num = 50;
 
-if (num == 20) {
-    console.log("num = 20");
-}
-else if (num > 15) {
-    console.log("num больше 15");
-} else {
-    console.log("мимо");
-}
+// if (num == 20) {
+//     console.log("num = 20");
+// }
+// else if (num > 15) {
+//     console.log("num больше 15");
+// } else {
+//     console.log("мимо");
+// }
 
 //TODO: с одним условием - тернарный оператор.
 
 // (num == 50) ? console.log("ok") : console.log("Error");
 
-const num2 = 40;
+// const num2 = 40;
 
-switch (num2) {
-    case 49:
-        console.log("Не верно");
-        break;
-    case 100:
-        console.log("Нет не так");
-        break;
-    case 40:
-        console.log("SUPER");
-        break;
-    default:
-        console.log("NOT HAPPY");
-        break;
-}
+// switch (num2) {
+//     case 49:
+//         console.log("Не верно");
+//         break;
+//     case 100:
+//         console.log("Нет не так");
+//         break;
+//     case 40:
+//         console.log("SUPER");
+//         break;
+//     default:
+//         console.log("NOT HAPPY");
+//         break;
+// }
 
 //--------------------------------------------------------------------
 
 //TODO: Lesson 14 цыклы.
-let num3 = 45;
+// let num3 = 45;
 
-while (num3 < 50) {
-    console.log(num3);
-    num3++;
-}
+// while (num3 < 50) {
+//     console.log(num3);
+//     num3++;
+// }
 //--------------------------------------------------------------------
 
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        //break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         //break;
+//         continue;
+//     }
+//     console.log(i);
+// }
 //--------------------------------------------------------------------
 
+//TODO:  Function : arrow func.
+
+// function showFirstMessage() {
+//     console.log("Hello world");
+// }
+// showFirstMessage();
+//-----------------------------------------
+
+//! Function DECLARATION
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 3;  // local 
+//     console.log(num);
+// }
+// showFirstMessage("Hello world!");
+//-----------------------------------------
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 5));
+//-----------------------------------------
+
+function ret() {
+    let num = 50;
+    //
+    //
+    return num;  // возврат обработанной переменной num.
+}
+const anotherNum = ret();  // записывает результат в переменную
+console.log(anotherNum);
+
+//-----------------------------------------
+
+//! FUNCTION EXPRESSION Создается только когда до нее доходит КОД . 
+//! Вызов только снизу. 
+
+const logger = function () {
+    console.log("ILJA");
+};
+logger();
+//-----------------------------------------
+
+//!  ARROW F
+
+const calculator = (a, b) => a + b;
+
+const calc2 = (a, b) => {
+    console.log("hi");
+    return a - b;
+};
+
+//-------------------------------------------- LESSON 17
+//TODO: String Methods 
+
+const str = "test"; //lowerCasse / UpperCase
+
+let result = str.toUpperCase();
+console.log(result); // TEST
+console.log(str); //test
+
+//---------------------------------------
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit")); // result = 5    (-1 нету сходства)
+
+const logg = "hello world Ilja!";
+console.log(logg.slice(6, 11)); //world (можно с 1 аргум (6)).
+
+console.log(logg.substring(6, 11)); //world
+console.log(logg.substr(6, 5));
+
+//----------------------------------------
+//TODO: NUMBER Methods
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test2 = "12.2px";
+console.log(parseInt(test2)); // 12 - сделал чисто из 12.2px
+console.log(parseFloat(test2));
+
+//----------------------------------------
 
 
